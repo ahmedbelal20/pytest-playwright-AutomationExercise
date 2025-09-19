730fb39055b6ef1a1943e1289c68a46813c5d6fc
Ahmed Belal <ahmed.belal.pro@outlook.com>
2025-09-19 19:40:55 +0300

    feat: add constants

---
+++ enums/constants.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Url(Enum):
+    BASE = "https://automationexercise.com/"
+    LOGIN = "https://automationexercise.com/login"
+    SIGNUP = "https://automationexercise.com/login"
+
+class Text(Enum):
+    BUTTON_SIGNUP = " Signup / Login"
+
+class Title(Enum):
+    HOME = "Automation Exercise"
+    LOGIN = "Automation Exercise - Signup / Login"
+    SIGNUP = "Automation Exercise - Signup / Login"
+    
